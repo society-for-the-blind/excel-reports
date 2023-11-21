e1ded215de1e232313023850947d8c3df74ec065
Attila Gulyas <toraritte@gmail.com>
2023-11-20 19:55:19 -0500

    save

--- ExcelReports/Library.fs
+++ ExcelReports/Library.fs
@@ -10,6 +10,9 @@ type ISheet            = SS.UserModel.ISheet
 type XSSFWorkbook      = XSSF.UserModel.XSSFWorkbook
 type CellAddress       = SS.Util.CellAddress
 type MissingCellPolicy = SS.UserModel.MissingCellPolicy
+type CellRangeAddress  = SS.Util.CellRangeAddress
+type CellReference     = SS.Util.CellReference
+
 
 /// <summary>
 /// Opens an Excel file and returns the workbook.
@@ -35,11 +38,6 @@ let updateCell (workbook: XSSFWorkbook) (sheetIndex: int) (rowIndex: int) (colum
 let updateCellValue (cell: ICell) (value: string) =
     cell.SetCellValue value
 
-let getCell (workbook: XSSFWorkbook) (sheetIndex: int) (rowIndex: int) (columnIndex: int) =
-    let sheet: ISheet = workbook.GetSheetAt(sheetIndex)
-    let row: IRow = sheet.GetRow(rowIndex)
-    row.GetCell(columnIndex, MissingCellPolicy.CREATE_NULL_AS_BLANK)
-
 let saveWorkbook (workbook: XSSFWorkbook) (filePath: string) =
     use fileStream: System.IO.FileStream =
         new System.IO.FileStream(
@@ -49,6 +47,16 @@ let saveWorkbook (workbook: XSSFWorkbook) (filePath: string) =
     workbook.Write(fileStream)
 
 // TODO move to a utility module
+// ------------------------------
+// TODO This needs to be a constrained type (and possibly renamed to
+// `CellRecord` or smth) to be able to convert back and forth between
+// NPOI's `CellAddress` and this type.
+// -> NOTE 2023-11-19_19-56
+//    Perhaps not. A constrained type would take a lot of time, and it now
+//    conveys that it may fail to become an NPOI `CellAddress`. Converting
+//    an NPOI `CellAddress` to a record will be simple, and it will be just
+//    that, a temporary record.
+// ------------------------------
 type ProspectiveCellAddress = {
     Column: string;
     Row: int
@@ -69,34 +77,105 @@ let getCellByAddress (sheet: ISheet) (cellAddress: CellAddress) =
     let row: IRow = sheet.GetRow(cellAddress.Row)
     row.GetCell(cellAddress.Column, MissingCellPolicy.CREATE_NULL_AS_BLANK)
 
-type PasteDirection = | Down | Right // | Up | Left
+// let getCell (workbook: XSSFWorkbook) (sheetIndex: int) (rowIndex: int) (columnIndex: int) =
+//     let sheet: ISheet = workbook.GetSheetAt(sheetIndex)
+//     let row: IRow = sheet.GetRow(rowIndex)
+//     row.GetCell(columnIndex, MissingCellPolicy.CREATE_NULL_AS_BLANK)
 
-let enterClients (sheet: ISheet) (startCellAddress: CellAddress) (direction: PasteDirection) =
-    let columnNumber = column.ColumnIndex
-    let rowNumber = column.RowIndex
-    let row: IRow = sheet.GetRow(rowNumber)
-    let cell: ICell = row.GetCell(columnNumber, MissingCellPolicy.CREATE_NULL_AS_BLANK)
-    let cellAddress: CellAddress = new CellAddress(columnName)
-    let rowNumber = cellAddress.Row
-    let columnNumber = cellAddress.Column
-    let row: IRow = sheet.GetRow(rowNumber)
-    let cell: ICell = row.GetCell(columnNumber, MissingCellPolicy.CREATE_NULL_AS_BLANK)
-    let cellStyle = cell.CellStyle
-    let dataValidationHelper = sheet.GetDataValidationHelper()
-    let dataValidationConstraint = dataValidationHelper.CreateExplicitListConstraint(clients |> List.toArray)
-    let dataValidation = dataValidationHelper.CreateValidation(dataValidationConstraint, cellAddress)
-    dataValidation.ShowErrorBox <- true
-    dataValidation.ErrorStyle <- DataValidation.ErrorStyle.STOP
-    dataValidation.CreateErrorBox("Error", "Please select a valid client from the dropdown list.")
-    sheet.AddValidationData(dataValidation)
-    cell.CellStyle <- cellStyle
+let getProspectiveCell (workbook: XSSFWorkbook) (sheetNumber: int) (prospectiveCellAddress: ProspectiveCellAddress) =
+    let sheet: ISheet = workbook.GetSheetAt(sheetNumber)
+    let cellAddress = getCellAddressOrThrow prospectiveCellAddress
+    getCellByAddress sheet cellAddress
+
+let getCell (workbook: XSSFWorkbook) (sheetNumber: int) (address: string) =
+    let sheet: ISheet = workbook.GetSheetAt(sheetNumber)
+    let cellAddress = new CellAddress (address)
+    getCellByAddress sheet cellAddress
+
+let findMergedRegionByCell (cell: ICell) =
+    let cellRef = new CellReference (cell)
+    cell.Sheet.MergedRegions
+    |> Seq.tryFind (fun region -> region.IsInRange cellRef)
+
+let findMergedRegionByStringAddress (workbook: XSSFWorkbook) (sheetNumber: int) (address: string) =
+    let cell = getCell workbook sheetNumber address
+    findMergedRegionByCell cell
+
+// PONDER
+// How to match the found MergedRegion for a cell with the data
+// validations "defined" ("applicable"?) for that cell?
+
+// let lofa (a: SS.Util.CellReference) = a.Row
+
+// let cellRangeAddresstoRowTuple (cellRangeAddress: CellRangeAddress) : CellAddress * CellAddress =
+//     ( (new CellAddress (cellRangeAddress.FirstRow, cellRangeAddress.FirstColumn)),
+//       (new CellAddress (cellRangeAddress.LastRow, cellRangeAddress.LastColumn))
+//     )
+
+// let isCellAddressInRange (cellRangeAddress: CellRangeAddress) (cellAddress: CellAddress) =
+//     if cellRangeAddress.FirstRow <> cellRangeAddress.LastRow then
+//         failwith "NOT IMPLEMENTED: Cell range address spans multiple rows."
+//     let (firstCellAddress, lastCellAddress) = cellRangeAddresstoCellAddressTuple cellRangeAddress
+//     let firstCellRow = firstCellAddress.Row
+//     let firstCellColumn = firstCellAddress.Column
+//     let lastCellRow = lastCellAddress.Row
+//     let lastCellColumn = lastCellAddress.Column
+//     let cellRow = cellAddress.Row
+//     let cellColumn = cellAddress.Column
+//     (firstCellRow <= cellRow && cellRow <= lastCellRow) &&
+//     (firstCellColumn <= cellColumn && cellColumn <= lastCellColumn)
 
 
 
+open Npgsql.FSharp
+
+let connectionString = "postgres://postgres:XntSrCoEEZtiacZrx2m7jR5htEoEfYyoKncfhNmnPrLqPzxXTU5nxM@192.168.64.4:5432/lynx"
+let q = connectionString |> Sql.connect |> Sql.query "select * from lynx_sipnote where id = 27555;" |> Sql.execute (fun read -> read.text "note")
+
+// type PasteDirection = | Down | Right // | Up | Left
+
+// let insertListDown (startCell: ICell) (list: string list): unit =
+// NOTE 2023-11-19_20-32 How to get the next cell in a column
+// > let a7 = getCellByAddress (o.GetSheetAt 3) (getCellAddressOrThrow { Column = "A"; Row = 7});;
+// val a7: ICell = Sample Joe Smith
+
+// > let a8address = new CellAddress ( (a7.Address.Row + 1), a7.Address.Column);;
+// val a8address: CellAddress = A8
+
+// > let a8 = getCellByAddress a7.Sheet a8address;;
+// val a8: ICell = Sample Ann Jones
+
+// let insertListRight (sheet: ISheet) (startCellAddress: CellAddress) (list: string list): unit =
+//     let row: IRow = sheet.GetRow(rowNumber)
+//     let cell: ICell = row.GetCell(columnNumber, MissingCellPolicy.CREATE_NULL_AS_BLANK)
+//     let cellAddress: CellAddress = new CellAddress(columnName)
+//     let rowNumber = cellAddress.Row
+//     let columnNumber = cellAddress.Column
+//     let row: IRow = sheet.GetRow(rowNumber)
+//     let cell: ICell = row.GetCell(columnNumber, MissingCellPolicy.CREATE_NULL_AS_BLANK)
+//     let cellStyle = cell.CellStyle
+//     let dataValidationHelper = sheet.GetDataValidationHelper()
+//     let dataValidationConstraint = dataValidationHelper.CreateExplicitListConstraint(clients |> List.toArray)
+//     let dataValidation = dataValidationHelper.CreateValidation(dataValidationConstraint, cellAddress)
+//     dataValidation.ShowErrorBox <- true
+//     dataValidation.ErrorStyle <- DataValidation.ErrorStyle.STOP
+//     dataValidation.CreateErrorBox("Error", "Please select a valid client from the dropdown list.")
+//     sheet.AddValidationData(dataValidation)
+//     cell.CellStyle <- cellStyle
+
+// let insertList (sheet: ISheet) (startCellAddress: CellAddress) (direction: PasteDirection) (list: string list): unit =
+//     match direction with
+//     | Down -> insertListDown sheet startCellAddress list
+//     | Right -> insertListRight sheet startCellAddress list
+
+// let enterClients (sheet: ISheet) (startCellAddress: CellAddress) (direction: PasteDirection) =
 
 
 
-
+let convertStringToInt (s: string) =
+    match System.Int32.TryParse(s) with
+    | true, i -> Some i
+    | _ -> None
 
 let getSheetDataValidations (workbook: XSSFWorkbook) (sheetIndex: int) =
     let sheet: ISheet = workbook.GetSheetAt(sheetIndex)
