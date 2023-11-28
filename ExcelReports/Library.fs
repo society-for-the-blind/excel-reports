e6390509bde3a801a74a085e334d2e01a3dbaece
Attila Gulyas <toraritte@gmail.com>
2023-11-28 12:35:16 -0500

    save

--- ExcelReports/Library.fs
+++ ExcelReports/Library.fs
@@ -12,7 +12,9 @@ type CellAddress       = SS.Util.CellAddress
 type MissingCellPolicy = SS.UserModel.MissingCellPolicy
 type CellRangeAddress  = SS.Util.CellRangeAddress
 type CellReference     = SS.Util.CellReference
-
+type AreaReference     = SS.Util.AreaReference
+type IDataValidation   = SS.UserModel.IDataValidation
+type ValidationType    = SS.UserModel.ValidationType
 
 /// <summary>
 /// Opens an Excel file and returns the workbook.
@@ -28,17 +30,20 @@ let openExcelFileWithNPOI (filePath: string) : XSSFWorkbook =
     let workbook = new XSSFWorkbook(fileStream)
     workbook
 
-let updateCell (workbook: XSSFWorkbook) (sheetIndex: int) (rowIndex: int) (columnIndex: int) (value: string) =
-    let sheet: ISheet = workbook.GetSheetAt(sheetIndex)
-    let row: IRow = sheet.GetRow(rowIndex)
-    let cell: ICell = row.GetCell(columnIndex, MissingCellPolicy.CREATE_NULL_AS_BLANK)
-    cell.SetCellValue value
+// let updateCell (workbook: XSSFWorkbook) (sheetIndex: int) (rowIndex: int) (columnIndex: int) (value: string) =
+//     let sheet: ISheet = workbook.GetSheetAt(sheetIndex)
+//     let row: IRow = sheet.GetRow(rowIndex)
+//     let cell: ICell = row.GetCell(columnIndex, MissingCellPolicy.CREATE_NULL_AS_BLANK)
+//     cell.SetCellValue value
 
-// TODO delete this at one point
-let updateCellValue (cell: ICell) (value: string) =
-    cell.SetCellValue value
+let updateCell (cell: ICell) (string: string) =
+    cell.SetCellValue string
+
+let recalculateFormulas (workbook: XSSFWorkbook) =
+    workbook.GetCreationHelper().CreateFormulaEvaluator().EvaluateAll()
 
 let saveWorkbook (workbook: XSSFWorkbook) (filePath: string) =
+    recalculateFormulas workbook
     use fileStream: System.IO.FileStream =
         new System.IO.FileStream(
             filePath,
@@ -46,61 +51,74 @@ let saveWorkbook (workbook: XSSFWorkbook) (filePath: string) =
         )
     workbook.Write(fileStream)
 
-// TODO move to a utility module
-// ------------------------------
-// TODO This needs to be a constrained type (and possibly renamed to
-// `CellRecord` or smth) to be able to convert back and forth between
-// NPOI's `CellAddress` and this type.
-// -> NOTE 2023-11-19_19-56
-//    Perhaps not. A constrained type would take a lot of time, and it now
-//    conveys that it may fail to become an NPOI `CellAddress`. Converting
-//    an NPOI `CellAddress` to a record will be simple, and it will be just
-//    that, a temporary record.
-// ------------------------------
-type ProspectiveCellAddress = {
-    Column: string;
-    Row: int
-}
-
-// TODO move to a utility module
-let maybeCellAddress ({ Column = col; Row = row}: ProspectiveCellAddress) :CellAddress option =
-    let npoiCellAddress = (new CellAddress (col + (string row)))
-    match npoiCellAddress with
-    | _ as c when c.Column = -1 -> None
-    | _ as c -> Some c
-
-// TODO move to a utility module
-let getCellAddressOrThrow : ProspectiveCellAddress -> CellAddress =
-    Option.get << maybeCellAddress
-
 let getCellByAddress (sheet: ISheet) (cellAddress: CellAddress) =
     let row: IRow = sheet.GetRow(cellAddress.Row)
     row.GetCell(cellAddress.Column, MissingCellPolicy.CREATE_NULL_AS_BLANK)
-
-// let getCell (workbook: XSSFWorkbook) (sheetIndex: int) (rowIndex: int) (columnIndex: int) =
-//     let sheet: ISheet = workbook.GetSheetAt(sheetIndex)
-//     let row: IRow = sheet.GetRow(rowIndex)
-//     row.GetCell(columnIndex, MissingCellPolicy.CREATE_NULL_AS_BLANK)
-
-let getProspectiveCell (workbook: XSSFWorkbook) (sheetNumber: int) (prospectiveCellAddress: ProspectiveCellAddress) =
-    let sheet: ISheet = workbook.GetSheetAt(sheetNumber)
-    let cellAddress = getCellAddressOrThrow prospectiveCellAddress
-    getCellByAddress sheet cellAddress
 
 let getCell (workbook: XSSFWorkbook) (sheetNumber: int) (address: string) =
     let sheet: ISheet = workbook.GetSheetAt(sheetNumber)
     let cellAddress = new CellAddress (address)
     getCellByAddress sheet cellAddress
 
-let findMergedRegionByCell (cell: ICell) =
+// NOTE 2023-11-27_10-24
+// Shelving this experiment to create a more meaningful type for cell
+// address strings, but not sure if it's worth to create an elaborate
+// constrained type just yet.
+//
+// // TODO move to a utility module
+// // ------------------------------
+// // TODO This needs to be a constrained type (and possibly renamed to
+// // `CellRecord` or smth) to be able to convert back and forth between
+// // NPOI's `CellAddress` and this type.
+// // -> NOTE 2023-11-19_19-56
+// //    Perhaps not. A constrained type would take a lot of time, and it now
+// //    conveys that it may fail to become an NPOI `CellAddress`. Converting
+// //    an NPOI `CellAddress` to a record will be simple, and it will be just
+// //    that, a temporary record.
+// // ------------------------------
+// type ProspectiveCellAddress = {
+//     Column: string;
+//     Row: int
+// }
+//
+// // // TODO move to a utility module
+// // let maybeCellAddress ({ Column = col; Row = row}: ProspectiveCellAddress) :CellAddress option =
+// //     let npoiCellAddress = (new CellAddress (col + (string row)))
+// //     match npoiCellAddress with
+// //     | _ as c when c.Column = -1 -> None
+// //     | _ as c -> Some c
+//
+// // // TODO move to a utility module
+// // let getCellAddressOrThrow : ProspectiveCellAddress -> CellAddress =
+// //     Option.get << maybeCellAddress
+//
+// let getProspectiveCell (workbook: XSSFWorkbook) (sheetNumber: int) (prospectiveCellAddress: ProspectiveCellAddress) =
+//     let sheet: ISheet = workbook.GetSheetAt(sheetNumber)
+//     let cellAddress = getCellAddressOrThrow prospectiveCellAddress
+//     getCellByAddress sheet cellAddress
+
+// NOTE Cells -> Row -> Sheet -> Workbook
+let findMergedRegion (cell: ICell) =
     let cellRef = new CellReference (cell)
     cell.Sheet.MergedRegions
-    |> Seq.tryFind (fun region -> region.IsInRange cellRef)
+    |> Seq.tryFind (fun (region: CellRangeAddress) -> region.IsInRange cellRef)
 
 let findMergedRegionByStringAddress (workbook: XSSFWorkbook) (sheetNumber: int) (address: string) =
-    let cell = getCell workbook sheetNumber address
-    findMergedRegionByCell cell
+    // (findMergedRegion << (getCell workbook sheetNumber)) address
+    let cell: ICell = getCell workbook sheetNumber address
+    findMergedRegion cell
 
+let findDataValidation (cell: ICell) =
+    let cellRef = new CellReference (cell)
+    cell.Sheet.GetDataValidations()
+    |> Seq.tryFind (fun (validation: SS.UserModel.IDataValidation) -> validation.Regions.CellRangeAddresses |> Seq.exists (fun region -> region.IsInRange cellRef))
+
+let findDataValidationByCellAddress (workbook: XSSFWorkbook) (sheetNumber: int) (address: string) : SS.UserModel.IDataValidation option =
+    // (findDataValidation << (getCell workbook sheetNumber)) address
+    let cell: ICell = getCell workbook sheetNumber address
+    findDataValidation cell
+
+// workbook.GetAllNames() = Status, Pending, Assessed named cell ranges
 // PONDER
 // How to match the found MergedRegion for a cell with the data
 // validations "defined" ("applicable"?) for that cell?
@@ -126,11 +144,14 @@ let findMergedRegionByStringAddress (workbook: XSSFWorkbook) (sheetNumber: int) 
 //     (firstCellColumn <= cellColumn && cellColumn <= lastCellColumn)
 
 
-
+// #r "nuget: Npgsql.FSharp, 5.7.0"
+// #r "nuget: SqlHydra.Query, 2.2.1";;
 open Npgsql.FSharp
 
 let connectionString = "postgres://postgres:XntSrCoEEZtiacZrx2m7jR5htEoEfYyoKncfhNmnPrLqPzxXTU5nxM@192.168.64.4:5432/lynx"
 let q = connectionString |> Sql.connect |> Sql.query "select * from lynx_sipnote where id = 27555;" |> Sql.execute (fun read -> read.text "note")
+
+
 
 // type PasteDirection = | Down | Right // | Up | Left
 
@@ -181,34 +202,60 @@ let getSheetDataValidations (workbook: XSSFWorkbook) (sheetIndex: int) =
     let sheet: ISheet = workbook.GetSheetAt(sheetIndex)
     sheet.GetDataValidations()
 
-let iterateThroughCellsInWorksheet (sheet: ISheet) (f: ICell -> unit) =
-    let rows = sheet.GetRowEnumerator()
-    while rows.MoveNext() do
-        let row = (rows.Current :?> IRow).GetEnumerator()
-        while row.MoveNext() do
-            let cell = row.Current
-            f cell
-
-let lazyIterateThroughCellsInWorksheet (sheet: ISheet) (f: ICell -> unit) =
-    let rows = sheet.GetRowEnumerator()
-    let rowSeq =
-        Seq.unfold
-            (fun (enumerator: System.Collections.IEnumerator) ->
-                if enumerator.MoveNext()
-                then Some(enumerator.Current :?> IRow, enumerator)
-                else None)
-            rows
-    rowSeq
-    |> Seq.iter (fun row ->
-        let cellEnumerator = row.GetEnumerator()
-        let cellSeq =
-            Seq.unfold
-                (fun (enumerator: System.Collections.IEnumerator) ->
-                    if enumerator.MoveNext()
-                    then Some(enumerator.Current :?> ICell, enumerator)
-                    else None)
-                cellEnumerator
-        cellSeq
-        |> Seq.iter f)
-
 //  (fun i -> let dv = (((s.GetDataValidations ()) |> Seq.toArray)[i]) in (dv.ValidationConstraint, dv.Regions.CellRangeAddresses)) 16;;
+
+// VALIDATIONS
+
+let getNamedRangeValues (workbook: XSSFWorkbook) (namedRangeName: string) =
+    let namedRange = workbook.GetName(namedRangeName)
+    let area = new AreaReference(namedRange.RefersToFormula, workbook.SpreadsheetVersion)
+    // NOTE 2023-11-22_0740
+    // These `cellRefs` retain reference to the sheet as well because
+    // `RefersToFormula` is essentially a workbook level property.
+    // cf. NOTE 2023-11-22_0743
+    let cellRefs = area.GetAllReferencedCells()
+    // let cellValues =
+    cellRefs
+    |> Array.map (fun cellRef ->
+        let sheet = workbook.GetSheet(cellRef.SheetName)
+        let cell = sheet.GetRow(cellRef.Row).GetCell((int cellRef.Col))
+        cell.StringCellValue
+    )
+    // Array.contains newValue cellValues
+
+let listWorksheets (workbook: XSSFWorkbook) =
+    [|for i in 0 .. workbook.NumberOfSheets - 1 -> workbook.GetSheetAt(i)|]
+
+let getAllDataValidations (workbook: XSSFWorkbook) =
+    listWorksheets workbook
+    |> Array.map (fun sheet ->
+        sheet.GetDataValidations()
+        |> Seq.toArray
+    )
+    |> Array.concat
+
+// NOTE 2023-11-24_0712
+// It was a conscious decision to focus only on list-type data
+// validations, and throw if there are more.
+// WHY? To curb complexity: https://stackoverflow.com/a/77518656/1498178
+// The gist is that there are at least 7 types of data validations, but
+// OIB reports only(?) use those - and list-type data validations are
+// complex enough themselves.
+let getDataValidationsForCell (cell: ICell)=
+    let sheet = cell.Sheet
+    sheet.GetDataValidations()
+    |> Seq.filter (fun dv ->
+        dv.Regions.CellRangeAddresses
+        |> Seq.exists (fun cellRangeAddress -> cellRangeAddress.IsInRange(new CellReference(cell)))
+    )
+    |> Seq.toArray
+
+// let getDataValidationValues (sheet: ISheet) (dv: IDataValidation) =
+
+//====================
+
+let a (s: SS.UserModel.IRichTextString) = 27
+let b (s: SS.Util.AreaReference) = 27
+let c (s: SS.Util.CellReference) = 27
+let d (s: XSSFWorkbook) = s.GetAllNames()
+let e (r: IRow) = 27
