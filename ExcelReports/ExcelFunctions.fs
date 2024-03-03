society-for-the-blind/excel-reports

commit 374322e9d5fcd524a881edf75cc6e81746ea1e27

[refactor] Separate OIB types and their printability

ExcelReports/ExcelFunctions.fs
@@ -25,8 +25,8 @@ type IndexedColors     = SS.UserModel.IndexedColors
 type FillPattern       = SS.UserModel.FillPattern
 
 type ColumnName = string
-type ParseResult = Result<IStringable, string>
-type ReportColumn = ColumnName * ParseResult
+type ReportCell = Result<System.IFormattable, string>
+type ReportColumn = ColumnName * ReportCell
 type ReportRow = ReportColumn list
 type ReportSheetData = ReportRow seq
 
@@ -158,10 +158,12 @@ let fillRow
     (cellTransforms: (NPOI.SS.UserModel.ICell -> unit) list)
     =
 
+    printfn "fillRow"
     let errorColor = hexStringToRGB "#ffc096"
     newRow
     |> Seq.iter (
         fun ((column, result): ReportColumn) ->
+            printfn "column: %s" column
             // let rowNum = string(i + 7)
             let cell = getCell workbook sheetNumber (column + rowNumber)
 
@@ -171,7 +173,7 @@ let fillRow
             let cellString =
                 match result with
                 | Ok reportValue ->
-                    stringify reportValue
+                    string reportValue
                 | Error str ->
                     changeCellColor cell errorColor
                     "Error: " + str
@@ -187,10 +189,12 @@ let populateSheet
     : XSSFWorkbook
     =
 
+    printfn "populateSheet"
     rows
     // |> Seq.sortBy extractClientName
     |> Seq.iteri (
         fun i row ->
+             printfn "populateSheet iteri"
              // TODO "sheet_start_row"
              //      The  numbe r 7  below  denotes  the  start  row from
              //      where  the  "rows"  is  `ReportSheetData`  need  to  be

ExcelReports/LynxData.fs
@@ -341,8 +341,8 @@ type Quarter =
     | Q3
     | Q4
 
-    interface IStringable with
-        member this.Stringify () =
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | Q1 -> "Q1"
             | Q2 -> "Q2"

ExcelReports/OIBTypes.fs
@@ -1,5 +1,7 @@
 module ExcelReports.OIBTypes
 
+// TODO rename to `OIBReportTypes`
+
 (*
 #load "ExcelReports/OIBTypes.fs";;
 open ExcelReports.OIBTypes;;
@@ -17,28 +19,36 @@ open ExcelReports.OIBTypes;;
 //     FSharpType.GetUnionCases(typeof<'T>)
 //     |> Array.map (fun caseInfo -> caseInfo.Name)
 
-type IStringable =
-    abstract member Stringify: unit -> string
+type IOIBType =
+    abstract member isOIBType: unit -> unit
+
+type OIBReportParseResult = Result<IOIBType, string>
+type OIBReportColumn = string * OIBReportParseResult
+type OIBReportRow = OIBReportColumn list
+type OIBReportSheetData = OIBReportRow seq
 
 type YesOrNo =
     | Yes
     | No
 
-    // Implement IStringable interface for yesOrNo
-    interface IStringable with
-        member this.Stringify() =
+    // Implement IOIBType interface for yesOrNo
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | Yes -> "Yes"
             | No  -> "No"
 
-let stringify (value: IStringable) =
-    value.Stringify ()
-
 type ClientName =
 | ClientName of string
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | ClientName name -> name
 
@@ -48,8 +58,11 @@ type IndividualsServed =
     | NewCase
     | PriorCase
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | NewCase   -> "Case open between Oct. 1 - Sept. 30"
             | PriorCase -> "Case open prior to Oct. 1"
@@ -67,8 +80,11 @@ type AgeAtApplication =
     | AgeBracket75To84
     | AgeBracket85AndOlder
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | AgeBracket18To24 -> "18-24"
             | AgeBracket25To34 -> "25-34"
@@ -86,9 +102,12 @@ type Gender =
     | Male
     | DidNotSelfIdentifyGender
 
-    // Implement IStringable interface for Gender
-    interface IStringable with
-        member this.Stringify() =
+    // Implement IOIBType interface for Gender
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | Female                   -> "Female"
             | Male                     -> "Male"
@@ -105,9 +124,12 @@ type Race =
     | DidNotSelfIdentifyRace
     | TwoOrMoreRaces
 
-    // Implement IStringable interface for Race
-    interface IStringable with
-        member this.Stringify() =
+    // Implement IOIBType interface for Race
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | NativeAmerican                  -> "American Indian or Alaska Native"
             | Asian                           -> "Asian"
@@ -131,9 +153,12 @@ type DegreeOfVisualImpairment =
     | LegallyBlind
     | SevereVisionImpairment
 
-    // Implement IStringable interface for DegreeOfVisualImpairment
-    interface IStringable with
-        member this.Stringify() =
+    // Implement IOIBType interface for DegreeOfVisualImpairment
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | TotallyBlind -> "Totally Blind"
             | LegallyBlind -> "Legally Blind"
@@ -149,9 +174,12 @@ type MajorCauseOfVisualImpairment =
     | Cataracts
     | OtherCausesOfVisualImpairment
 
-    // Implement IStringable interface for MajorCauseOfVisualImpairment
-    interface IStringable with
-        member this.Stringify() =
+    // Implement IOIBType interface for MajorCauseOfVisualImpairment
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | MacularDegeneration           -> "Macular Degeneration"
             | DiabeticRetinopathy           -> "Diabetic Retinopathy"
@@ -192,9 +220,12 @@ type TypeOfResidence =
     | NursingHome
     | Homeless
 
-    // Implement IStringable interface for TypeOfResidence
-    interface IStringable with
-        member this.Stringify() =
+    // Implement IOIBType interface for TypeOfResidence
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | PrivateResidence          -> "Private Residence"
             | SeniorIndependentLiving   -> "Senior Independent Living"
@@ -221,9 +252,12 @@ type SourceOfReferral =
     | SelfReferral
     | Other
 
-// Implement IStringable interface for SourceOfReferral
-    interface IStringable with
-        member this.Stringify() =
+// Implement IOIBType interface for SourceOfReferral
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | EyeCareProvider           -> "Eye Care Provider"
             | PhysicianMedicalProvider  -> "Physician/ Medical Provider"
@@ -260,8 +294,11 @@ type County =
     | Sutter        | Tehama       | Trinity      | Tulare     | Tuolumne
     | Ventura       | Yolo         | Yuba
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | ContraCosta    -> "Contra Costa"
             | DelNorte       -> "Del Norte"
@@ -363,24 +400,27 @@ type County =
 // type PlanDate =
 //     | PlanDate of System.DateOnly
 
-//     interface IStringable with
-//         member this.Stringify() =
+//     interface IOIBType with
+//         member this.isOIBType() =
 //             match this with
 //             | PlanDate date -> date.ToString()
 
 // type PlanId =
 //     | PlanId of int
 
-//     interface IStringable with
-//         member this.Stringify() =
+//     interface IOIBType with
+//         member this.isOIBType() =
 //             match this with
 //             | PlanId id -> string(id)
 
 type PlanModified =
     | PlanModified of System.DateTime
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | PlanModified dt -> dt.ToString()
 
@@ -400,8 +440,11 @@ type OutcomeA =
     | AssessedAndMaintainedIndependence
     | AssessedWithDecreasedIndependence
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | NotAssessed                        -> "Not assessed"
             | AssessedWithImprovedIndependence   -> "Assessed with improved independence"
@@ -470,8 +513,11 @@ type CaseStatus = // "V7"
     | Assessed
     | Pending
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | Assessed   -> "Assessed"
             | Pending -> "Pending"
@@ -482,8 +528,11 @@ type OutcomeB =
     | Decreased
     | NotAssessed
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | Increased -> "Increased"
             | Maintained -> "Maintained"
@@ -502,8 +551,11 @@ type EmploymentOutcomes = // "AE7:AH7"
     | Unsure
     | MoreLikely
 
-    interface IStringable with
-        member this.Stringify() =
+    interface IOIBType with
+        member this.isOIBType() = ()
+
+    interface System.IFormattable with
+        member this.ToString(_format: string, _formatProvider: System.IFormatProvider) =
             match this with
             | NotInterested -> "Not Interested"
             | LessLikely -> "Less Likely"

commit fa80cb985c33e29b0a17e74c2c504efdd72b9a21

Fix bug introduced in 374322e [..]
Before  374322e, `OIBTypes`  have been  converted to
strings using a custom  interface, and when switched
to   `System.IFormattable`,   the  `County`   type's
`ToString`  implementation  introduced  an  infinite
loop when trying to take a shortcut to manually write
out each type member.

In the end, the fix was to manually enumerate each member..:)

ExcelReports/ExcelFunctions.fs
@@ -58,14 +58,11 @@ open System.Reflection
 
 let cloneCellStyle (cell: NPOI.XSSF.UserModel.XSSFCell)  =
     let original = cell.CellStyle
-    // printfn "ORIGINAL: %A" original.FontIndex
     let workbook = cell.Sheet.Workbook
     let copy = workbook.CreateCellStyle()
     let properties = original.GetType().GetProperties(BindingFlags.Public ||| BindingFlags.Instance)
     for prop in properties do
-        // printfn "MAIN: %s --- %A" prop.Name (prop.GetValue(original))
         if prop.CanRead && prop.CanWrite then
-            // printfn "IF: %s --- %A" prop.Name (prop.GetValue(original))
             let value = prop.GetValue(original)
             prop.SetValue(copy, value)
     // `FontIndex` can only be set by the `SetFont` method.
@@ -158,13 +155,10 @@ let fillRow
     (cellTransforms: (NPOI.SS.UserModel.ICell -> unit) list)
     =
 
-    printfn "fillRow"
     let errorColor = hexStringToRGB "#ffc096"
     newRow
     |> Seq.iter (
         fun ((column, result): ReportColumn) ->
-            printfn "column: %s" column
-            // let rowNum = string(i + 7)
             let cell = getCell workbook sheetNumber (column + rowNumber)
 
             // Some cells in a template require special care; see `generateQuarterlyReport` in `ExcelReports.fs`.
@@ -189,12 +183,10 @@ let populateSheet
     : XSSFWorkbook
     =
 
-    printfn "populateSheet"
     rows
     // |> Seq.sortBy extractClientName
     |> Seq.iteri (
         fun i row ->
-             printfn "populateSheet iteri"
              // TODO "sheet_start_row"
              //      The  numbe r 7  below  denotes  the  start  row from
              //      where  the  "rows"  is  `ReportSheetData`  need  to  be

ExcelReports/Library.fs
@@ -16,7 +16,10 @@ open ExcelReports.LynxData;;
 open ExcelReports.OIB;;
 
 -- oneliner:
-#r "nuget: NPOI, 2.6.2";; #load "ExcelReports/OIBTypes.fs";; open ExcelReports.OIBTypes;; #load "ExcelReports/ExcelFunctions.fs";; open ExcelReports.ExcelFunctions;; #r "nuget: Npgsql.FSharp, 5.7.0";; #load "ExcelReports/LynxData.fs";; open ExcelReports.LynxData;; #load "ExcelReports/Library.fs";; open ExcelReports.OIB;;
+fsi.ShowDeclarationValues <- false;;
+ #r "nuget: NPOI, 2.6.2";; #load "ExcelReports/OIBTypes.fs";; open ExcelReports.OIBTypes;; #load "ExcelReports/ExcelFunctions.fs";; open ExcelReports.ExcelFunctions;; #r "nuget: Npgsql.FSharp, 5.7.0";; #load "ExcelReports/LynxData.fs";; open ExcelReports.LynxData;; #load "ExcelReports/Library.fs";; open ExcelReports.OIB;;
+
+let conn2 = "postgres://postgres:password@192.168.64.4:5432/lynx";;
 
 generateQuarterlyReport conn2 OIB_7OB    2023 Q2 "dev";;
 generateQuarterlyReport conn2 OIB_Non7OB 2023 Q2 "dev";;
@@ -304,7 +307,6 @@ let getColumn
     (lynxColumn: string option)
     : OIBReportParseResult =
 
-    printfn "getColumn"
     // See NOTE "FS0025: Incomplete pattern match" warning above
     match lynxColumn with
     | OIBCase columnType nonOIBDefault result ->
@@ -491,8 +493,8 @@ let mapToDemographicsRow
     (row: QuarterlyReportQueryRow)
     : OIBReportRow =
 
-    printfn "mapToDemographicsRow"
-    [ ( "A", (getClientName row) )
+    [
+      ( "A", (getClientName row) )
     ; ( "B", getIndividualsServed row            grantYearStart )
     ; ( "E", getAgeAtApplication  row reportType grantYearEnd )
     ; ( "J", getColumnCached typeof<Gender> None row.intake_gender )
@@ -526,17 +528,20 @@ let groupLynxRowsByClientName (rows: QuarterlyReportQueryRow seq) =
 
 let getTabData
     (toOIBRows: QuarterlyReportQueryRow -> OIBReportRow )
-    (lynxData: OIBQuarterlyReportData)
+    (queryData: OIBQuarterlyReportData)
     : OIBRowsGroupedAndOrderedByClientName =
 
-    lynxData.lynxData
-    |> Seq.map (fun (row: ISQLQueryColumnable) -> row :?> QuarterlyReportQueryRow)
+    queryData.lynxData
+    |> Seq.map (fun (row: ISQLQueryColumnable) ->
+        row :?> QuarterlyReportQueryRow
+     )
     |> groupLynxRowsByClientName
     |> Seq.sortBy fst
     |> Seq.map (fun (client: Client, lynxRows) ->
         lynxRows
         |> Seq.map toOIBRows
-        |> fun oibRows -> (client, oibRows)
+        |> fun oibRows ->
+            (client, oibRows)
        )
 
 let toReportColumn
@@ -557,16 +562,16 @@ let toReportRow (oibRow: OIBReportRow) : ReportRow =
 
 let getDemographics
     (reportType: QuarterlyOIBReportType)
-    (lynxData: OIBQuarterlyReportData)
+    (queryData: OIBQuarterlyReportData)
     : ReportSheetData
     =
 
-    lynxData
+    queryData
     |> getTabData
         ( mapToDemographicsRow
             reportType
-            lynxData.grantYearStart
-            lynxData.grantYearEnd
+            queryData.grantYearStart
+            queryData.grantYearEnd
         )
     |> Seq.map (fun ((_clientName, oibRows): ClientOIBRows) ->
         oibRows
@@ -582,9 +587,6 @@ let getDemographics
         //      for a one element list.
         |> Seq.exactlyOne
     )
-    |> fun e ->
-        printfn "leaving getDemographics"
-        e
 
 // ---SERVICES---------------------------------------------------------
 
@@ -706,8 +708,8 @@ let mergeOIBColumns
         | :? IOIBOutcome ->
             (colNameA, okA)
 
-let getServices (lynxData: OIBQuarterlyReportData) : ReportSheetData =
-    lynxData
+let getServices (queryData: OIBQuarterlyReportData) : ReportSheetData =
+    queryData
     |> getTabData mapToServicesRow
     |> Seq.map (fun (_clientName: Client, oibRows: OIBReportRow seq) ->
         oibRows
@@ -844,7 +846,6 @@ let generateQuarterlyReport
     let cellTransforms =
         // Sometimes using a previously generated report as a template, and error highlights need to be cleared - except for "Case Status" (column V) as it has a "default" color set by DOR.
         // TODO: implement setting "Case Status" and not setting it to "Assessed" indiscriminately.
-        printfn "cellTransforms"
         [ fun (cell: NPOI.SS.UserModel.ICell) ->
             if (cell.Address.Column <> 21)
             then resetCellColor cell

ExcelReports/LynxData.fs
@@ -385,6 +385,7 @@ let rowReaderBuilder<'T when 'T :> ISQLQueryColumnable>
     =
     // let getRecordFieldNamesAndTypes<'T when 'T :> ISQLQueryColumnable>
 
+    // printfn "rowReaderBuilder"
     let rowSpec: LynxQueryRowSpecification =
         getRecordFieldNamesAndTypes<'T>
 

ExcelReports/OIBTypes.fs
@@ -314,7 +314,50 @@ type County =
             | SantaBarbara   -> "Santa Barbara"
             | SantaClara     -> "Santa Clara"
             | SantaCruz      -> "Santa Cruz"
-            | _              -> string this
+            | Alameda -> "Alameda"
+            | Alpine -> "Alpine"
+            | Amador -> "Amador"
+            | Butte -> "Butte"
+            | Calaveras -> "Calaveras"
+            | Colusa -> "Colusa"
+            | Fresno -> "Fresno"
+            | Glenn -> "Glenn"
+            | Humboldt -> "Humboldt"
+            | Imperial -> "Imperial"
+            | Inyo -> "Inyo"
+            | Kern -> "Kern"
+            | Kings -> "Kings"
+            | Lake -> "Lake"
+            | Lassen -> "Lassen"
+            | Madera -> "Madera"
+            | Marin -> "Marin"
+            | Mariposa -> "Mariposa"
+            | Mendocino -> "Mendocino"
+            | Merced -> "Merced"
+            | Modoc -> "Modoc"
+            | Mono -> "Mono"
+            | Monterey -> "Monterey"
+            | Napa -> "Napa"
+            | Nevada -> "Nevada"
+            | Orange -> "Orange"
+            | Placer -> "Placer"
+            | Plumas -> "Plumas"
+            | Riverside -> "Riverside"
+            | Sacramento -> "Sacramento"
+            | Shasta -> "Shasta"
+            | Sierra -> "Sierra"
+            | Siskiyou -> "Siskiyou"
+            | Solano -> "Solano"
+            | Sonoma -> "Sonoma"
+            | Stanislaus -> "Stanislaus"
+            | Sutter -> "Sutter"
+            | Tehama -> "Tehama"
+            | Trinity -> "Trinity"
+            | Tulare -> "Tulare"
+            | Tuolumne -> "Tuolumne"
+            | Ventura -> "Ventura"
+            | Yolo -> "Yolo"
+            | Yuba -> "Yuba"
 // "BF7"
 
 // TODO 2023-12-18_2336